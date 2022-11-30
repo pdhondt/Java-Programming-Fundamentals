d65f35dc7142e4c255d9f752f85e024a0e566989
Peter D'hondt <dhondt.peter@gmail.com>
2022-11-30 11:18:35 +0100

    Initial Commit

---
+++ src/be/vdab/jpfhfdst02/HelloWorld.java
@@ -0,0 +1,9 @@
+package be.vdab.jpfhfdst02;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/be/vdab/jpfhfdst03/HelloGit.java
@@ -0,0 +1,9 @@
+package be.vdab.jpfhfdst03;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+
+}
